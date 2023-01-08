395891d29ebc33fc68790a2a2a113b4d9bfa68f5
frajibe <frajibe@protonmail.com>
2023-01-08 20:58:12 +0100

    perf: use an all-new endpoint to clear the playlists

--- piped/dto/video/DeletePlaylistVideoDto.go
+++
@@ -1,8 +0,0 @@
-// Package video provides the Dto related to the Piped video/steam.
-package video
-
-// DeletePlaylistVideoDto represents the request payload needed to remove a video from a playlist using the Piped Api.
-type DeletePlaylistVideoDto struct {
-	PlaylistId string `json:"playlistId"`
-	Index      int    `json:"index"`
-}
